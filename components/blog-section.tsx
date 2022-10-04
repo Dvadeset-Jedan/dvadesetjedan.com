d7782fe23cb38dd8d8c3ed3752684b0693a27703
djo1e <djordjespasic7@gmail.com>
2022-10-04 19:42:03 +0200

    Fix mobile width issue, improve blog section

--- components/blog-section.tsx
+++ components/blog-section.tsx
@@ -13,11 +13,12 @@ export function BlogSection({ posts }: { posts: { content: string; frontmatter: 
 
   return (
     <section className="mt-32">
-      <h2 className="text-[3.5rem] font-medium text-center">From the blog</h2>
-      {/* {posts.splice(0, 3).map(({ frontmatter }, index) => ( */}
-      {/*   <SmallerBlogPreview key={index} {...frontmatter} /> */}
-      {/* ))} */}
-      {/* <SmallerBlogPreview title="author" author={""} translator={""} meta={""} slug={""} /> */}
+      <h2 className="text-[3.5rem] mb-14 font-medium text-center">From the blog</h2>
+      <div className="grid grid-cols-1 gap-y-20 sm:gap-x-20 sm:grid-cols-2 lg:grid-cols-3">
+        {posts.splice(0, 3).map(({ frontmatter }, index) => (
+          <SmallerBlogPreview key={index} {...frontmatter} />
+        ))}
+      </div>
     </section>
   );
 }
