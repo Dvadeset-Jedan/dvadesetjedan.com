458229e346fa99c00834b512b3babb971106a472
djo1e <djordjespasic7@gmail.com>
2022-10-18 22:05:45 +0200

    Add link to blog image and heading

--- components/blog-preview.tsx
+++ components/blog-preview.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { images } from "../utils/images";
 import { routes } from "../utils/routes";
 
@@ -12,21 +13,29 @@ interface BlogPreviewProps {
 }
 
 export function SmallerBlogPreview({ title, author, translator, slug, meta }: BlogPreviewProps) {
+  const router = useRouter();
+
   return (
     <article className="flex flex-col">
       <div>
         <Image
           src={images.blogPreview}
-          className="rounded-2xl"
+          className="cursor-pointer rounded-2xl"
           width="320"
           height="200"
           layout="responsive"
           alt=""
           priority
+          onClick={() => router.push(routes.blogArticle(slug))}
         />
       </div>
       <div className="mt-6">
-        <h1 className="text-2xl font-medium md:text-2xl">{title}</h1>
+        <h1
+          className="text-2xl font-medium cursor-pointer md:text-2xl"
+          onClick={() => router.push(routes.blogArticle(slug))}
+        >
+          {title}
+        </h1>
         <p className="mt-4 text-lg md:text-md text-gray">
           Written by <span className="text-purple">{author}</span>, translated by{" "}
           <span className="text-purple">{translator}</span>
