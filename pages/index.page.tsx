9e67695a33c4f9ea826ba0f7c1fd77afd93b6991
matija-p9 <59355738+matija-p9@users.noreply.github.com>
2022-10-31 10:22:25 +0100

    Update text, remove picture

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -17,7 +17,7 @@ export default function Index({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <main className="bg-dark">
-      <Image
+      <!-- <Image
         src={images.bitcoinIsland}
         width={1440}
         height={720}
@@ -25,17 +25,32 @@ export default function Index({
         priority
         alt=""
         objectFit="cover"
-      />
+      /> -->
       <div className="pb-16 lg:pb-20 w-[90%] lg:w-[85%] 2xl:w-[75%] mx-auto">
         <div className="flex justify-center">
           <div className="text-center">
             <h1 className="mt-10 text-3xl font-semibold md:text-6xl lg:mt-28">
-              DvadesetJedan je balkanska bitcoin zajednica
+              DvadesetJedan<br>
+			  Balkanski Bitcoineri
             </h1>
             <p className="text-xl font-medium md:text-2xl mt-9">
-              DvadesetJedan je neformalna grupa bitcoin entuzijasta sa ex-yu prostora koja za cilj
-              ima edukaciju, podsticanje preduzetništva i širenje bitcoin mreže kroz različite
-              projekte.
+              Bitcoin je revolucionaran. Stvoren je da bi promijenio svijet. Živimo u vremenu transformacije.<br>
+			  DvadesetJedan je grupa otvorena svim Bitcoinerima.
+            </p>
+			<p className="text-xl font-medium md:text-2xl mt-9">
+			Što DvadesetJedan radi i koji su nam ciljevi?<br>
+			• Obrazovanje o Bitcoinu, kroz originalne i prevedene materijale<br>
+			• Poticanje i podrška lokalnog Bitcoin poduzetništva<br>
+			• Projekti usmjereni ka širenju Bitcoin mreže na svim razinama
+			</p>
+			<p className="text-xl font-medium md:text-2xl mt-9">
+			Kako nam se možeš pridružiti? Nekoliko je načina:<br>
+			• Uživo, na <a href="https://dvadeset-jedan.github.io/dvadesetjedan.com/meetups">jednom od druženja</a><br>
+			• <a href="https://t.me/dvadesetjedan21">Telegram</a> i <a href="https://discord.gg/D7ekwFFnDH">Discord</a><br>
+			• Ili prvo baci pogled na naš <a href="https://dvadeset-jedan.github.io/dvadesetjedan.com/podcast">podcast</a> i <a href="https://dvadeset-jedan.github.io/dvadesetjedan.com/blog">blog</a>
+			</p>
+			<p className="text-xl font-medium md:text-2xl mt-9">
+              Bitcoin je revolucionaran. I ovo je tek početak.
             </p>
           </div>
         </div>
