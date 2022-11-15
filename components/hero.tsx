bd6f34bac5abb9afa22c332ccd32e697e6b08d39
Djo1e <djordjespasic7@gmail.com>
2022-11-15 21:15:34 +0100

    Make hero images behave like photoswipe lib

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,23 +3,23 @@ import { images } from "../utils/images";
 
 export function Hero() {
   return (
-    <div className="md:grid md:grid-cols-2 mt-12">
+    <div className="grid grid-cols-2 mt-12">
       <Image
         src={images.hero1}
         width={561}
         height={561}
         layout="responsive"
         alt=""
-        className="rounded-2xl mr-4"
+        className="rounded-md md:rounded-2xl mr-4"
       />
-      <div className="hidden md:grid grid-cols-2 gap-4 ml-4">
+      <div className="grid grid-cols-2 gap-1 md:gap-4 ml-1 md:ml-4">
         <Image
           src={images.hero2}
           width={272}
           height={272}
           layout="responsive"
           alt=""
-          className="rounded-2xl"
+          className="rounded-md md:rounded-2xl"
         />
         <Image
           src={images.hero3}
@@ -27,7 +27,7 @@ export function Hero() {
           height={272}
           layout="responsive"
           alt=""
-          className="rounded-2xl"
+          className="rounded-md md:rounded-2xl"
         />
         <Image
           src={images.hero4}
@@ -35,7 +35,7 @@ export function Hero() {
           height={272}
           layout="responsive"
           alt=""
-          className="rounded-2xl"
+          className="rounded-md md:rounded-2xl"
         />
         <Image
           src={images.hero5}
@@ -43,7 +43,7 @@ export function Hero() {
           height={272}
           layout="responsive"
           alt=""
-          className="rounded-2xl"
+          className="rounded-md md:rounded-2xl"
         />
       </div>
     </div>
