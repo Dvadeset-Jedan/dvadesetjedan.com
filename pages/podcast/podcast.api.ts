1ce3b674ade91acf01a9c44a9ad0036ebb5f9738
Djo1e <djordjespasic7@gmail.com>
2022-11-17 15:23:56 +0100

    fix

--- pages/podcast/podcast.api.ts
+++ pages/podcast/podcast.api.ts
@@ -4,14 +4,14 @@ import Parser from "rss-parser";
 import { getSlug } from "./index.page";
 import { AnchorRSSReturnType, Episode } from "./podcast.types";
 
-const ANCHOR_ENDPOINT = "https://anchor.fm/s/962ca164/podcast/rss";
+const ANCHOR_ENDPOINT_URL = "https://anchor.fm/s/962ca164/podcast/rss";
 
 type UsePodcastEpisodeProps = {
   episodeSlug?: string;
 };
 
 export async function fetchPodcastEpisodes() {
-  const feed = (await new Parser().parseURL(ANCHOR_ENDPOINT)) as any;
+  const feed = (await new Parser().parseURL(ANCHOR_ENDPOINT_URL)) as any;
   return feed as AnchorRSSReturnType;
 }
 
