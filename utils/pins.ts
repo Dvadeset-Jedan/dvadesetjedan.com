d58db855cba3235855a100d1497cdd57049a095c
Djo1e <djordjespasic7@gmail.com>
2022-11-21 21:48:54 +0100

    Fix map, update contributing

---
+++ utils/pins.ts
@@ -0,0 +1,13 @@
+export const PINS = [
+  {
+    city: "Belgrade",
+    mobilePosition: {
+      x: 135,
+      y: 185,
+    },
+    desktopPosition: {
+      x: 760,
+      y: 305,
+    },
+  },
+];
