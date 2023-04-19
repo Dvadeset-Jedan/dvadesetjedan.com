2bcc1ca91e146efd1a9c70233ce4d1cfb79a5340
Djo1e <34274884+Djo1e@users.noreply.github.com>
2023-04-20 03:06:23 +0700

    Update discord invite link, remove past meetings, meetings map (#67)

--- utils/urls.ts
+++ utils/urls.ts
@@ -1,7 +1,8 @@
 export const urls = {
-  discord: "https://discord.gg/HjmfXq5Z",
+  discord: "https://discord.com/invite/WSNA28kd",
   youtube: "https://www.youtube.com/channel/UCYXly5XM_SO07vLHh2yPlCw",
   spotify: "https://open.spotify.com/show/0K4Ak2oNQdCU3Tsr3Tajkx?si=34af43f494d3487b",
   github: "https://github.com/Dvadeset-Jedan",
   telegram: "https://t.me/dvadesetjedan21",
+  meetup: "https://www.meetup.com/dvadeset-jedan",
 } as const;
